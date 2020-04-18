8f662eabd6445163e9396618e49ca43f4b828277
Oberon Lai <m615926@gmail.com>
2020-04-18 11:23:40 +0800

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit;}
+get_header();
+?>
+
+<div class="uk-container">
+	<h1 class="uk-text-center heading-medium">這是首頁</h1>
+</div>
+
+<?php
+get_footer();
---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+UIkit.use(Icons);
+
+import { Variable } from "./libs/Variable"; 
+const vars = new Variable();
+
+document.addEventListener('DOMContentLoaded',()=>{
+  vars.logo.classList.add('logo-class');
+})
